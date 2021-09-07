df4d6d74bf3bf60c3c19491f8e250e3a354f02ff
mahumrana <mrana@newtech.coppellisd.com>
2021-09-07 14:46:38 -0500

    Update Notes01_Printing_and_Escape_Sequences.java

--- src/notes/Notes01_Printing_and_Escape_Sequences.java
+++ src/notes/Notes01_Printing_and_Escape_Sequences.java
@@ -1,11 +1,16 @@
 package notes;
 
+/*
+ * Notes on how to print to the screen
+ * Also includes Escape Sequence
+ * 
+ */
 public class Notes01_Printing_and_Escape_Sequences {
 
 	public static void main(String[] args) {
 		
 		
-		
+System.out.print("sup yo");
 	}
 	
 	
